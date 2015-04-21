2df23a7816f8dd3effbd9a225fc8d70f2ba50f84
Yanhe <yanhe.liu@cs.helsinki.fi>
2015-04-21 16:33:29 +0300

    Add conf-generator and update README

--- README.md
+++ README.md
@@ -4,32 +4,44 @@ SoftOffload Agent is a Click-Based management module designed for wireless assoc
 
 Our agent logic is implemented mainly by using a custom Click element called [sdnagent](https://github.com/TKTL-SDN/SoftOffload-Agent/tree/eit-sdn/elements/local/agent/). To facilitate system building, we merge all the rest [Click codes](https://github.com/kohler/click) in this repo.
 
-# Usage
+## Usage
 
-## Build
+### Build
 
-    git clone https://github.com/TKTL-SDN/SoftOffload-Agent.git
-    cd SoftOffload-Agent
+    $ git clone https://github.com/TKTL-SDN/SoftOffload-Agent.git
+    $ cd SoftOffload-Agent
 
     # our current agent does not support kernel mode
     # you may choose to disable linux kernel mode to speed up the building
 
-    ./configure --enable-wifi --enable-local (--disable-linuxmodule)
-    make
+    $ ./configure --enable-wifi --enable-local (--disable-linuxmodule)
+    $ make
 
-## Generate custom configuration for your system
+### Generate custom configuration for your system
 
-Change agent [configuration](https://github.com/TKTL-SDN/SoftOffload-Agent/tree/eit-sdn/conf/local-agent/agent.click) for your system.
+Change agent configuration [agent.click](https://github.com/TKTL-SDN/SoftOffload-Agent/tree/eit-sdn/conf/local-agent) for your system.
 
-We will add a config generator script, and explain it later
+We provide a script to help you generate suitable config file quickly for your system.
 
-## Run
+```
+$ cd SoftOffload-Agent/conf/local-agent/
+
+# read our help instruction on parameter setting    
+$ ./agent-config-generator.py -h
+
+# run the script with suitable arguments to generate config file
+$ ./agent-config-generator.py [your args...] > agent.click
+```
+
+You can find instruction about parameters with this script when you run it, or you can use `-h` to check help info.
+
+
+### Run
 
     # run Click in the userlevel
-    sudo ./userlevel/click conf/local-agent/agnet.click
+    sudo ./userlevel/click conf/local-agent/agent.click
 
-# Licence
+## Licence
 
-This project is licensed under the Click LICENSE (based on an MIT license).
-
+This project is licensed under the [Click LICENSE](https://github.com/TKTL-SDN/SoftOffload-Agent/tree/eit-sdn/LICENSE) (based on an MIT license).
 
---
+++ conf/local-agent/agent-config-generator.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python
+import argparse
+import sys
+
+# parse arguments with argparse package
+parser = argparse.ArgumentParser(description='description: '
+    'config generator for SoftOffload agent')
+parser.add_argument('--mac', dest='mac', type=str, required=True, 
+    help='wireless interface MAC address of this AP, e.g. '
+        + '"00:00:00:00:00:01"')
+
+# dhcp
+parser.add_argument('--gateway_ip', dest='gateway_ip', type=str, 
+    default="192.168.0.1", help='IP string for gateway of this AP, e.g. '
+        + '"192.168.0.1"')
+parser.add_argument('--mask', dest='mask', type=str, 
+    default="255.255.255.0", help="DHCP address mask")
+parser.add_argument('--dns', dest='dns', type=str, 
+    default="8.8.8.8", help="DHCP dns option")
+parser.add_argument('--start_ip', dest='start', type=str, 
+    default="192.168.0.100", help="DHCP lease start IP")
+parser.add_argument('--end_ip', dest='end', type=str, 
+    default="192.168.0.250", help="DHCP lease end IP")
+
+# agent
+parser.add_argument('--agent_port', dest='agent_port', type=int, 
+    default=6777, help="agent listening port for messages from master")
+parser.add_argument('--agent_ip', dest='agent_ip', type=str, required=True, 
+    help="agent src IP address for communicate to master")
+parser.add_argument('--interval', dest='interval', type=int, 
+    default=2, help="interval (second) to report agent statistics to master")
+
+# master
+parser.add_argument('--master_port', dest='master_port', type=int, 
+    default=26284, help="udp port on master")
+parser.add_argument('--master_ip', dest='master_ip', type=str, required=True, 
+    help="master IP address, e.g. 192.168.3.1")
+
+# device
+parser.add_argument('--dev', dest='dev', type=str, required=True, 
+    help="dev name for the wireless interface, e.g. wlan1")
+parser.add_argument('--mon', dest='mon', type=str, required=True, 
+    help="monitoring dev name for the wireless interface, e.g. mon.wlan1")
+
+args = parser.parse_args()
+
+# print config
+print """// agent config
+
+// dhcp ip lease pool for this AP
+leases :: LeasePool( ETH %s, IP %s, 
+                    MASK %s, DNS %s, 
+                    START %s, END %s );
+""" % (args.mac, args.gateway_ip, args.mask, args.dns, args.start, args.end)
+
+print """
+// local offload agent
+sdn_agent::SdnAgent(MAC %s, CTRL_IP %s, 
+                    AP_IP %s, INTERVAL %d, 
+                    LEASES leases,
+                    SHELL_PATH "this is not required anymore");
+
+// receive messages from master server on port %d
+agent_socket::Socket(UDP, 0.0.0.0, %d)
+    -> [0]sdn_agent;
+
+// send packets to master server
+sdn_agent[0] -> master_socket::Socket(UDP, %s, %d);
+""" % (args.mac, args.agent_ip, args.gateway_ip, args.interval, 
+        args.agent_port, args.agent_port, args.master_ip, args.master_port)
+
+print """
+// send packets from agent to client
+sdn_agent[1]
+    -> q::Queue(1000)
+    -> to_dev::ToDevice(%s);
+
+// sniffer 802.11 packet
+FromDevice(%s)
+// -> prism2_decap :: Prism2Decap()
+// -> extra_decap :: ExtraDecap()
+// -> AthdescDecap()
+    -> RadiotapDecap()
+    -> extra_decap :: ExtraDecap()
+    -> phyerr_filter :: FilterPhyErr()
+    -> tx_filter :: FilterTX()
+    -> dupe :: WifiDupeFilter() 
+    -> wifi_cl :: Classifier(0/08%%0c 1/01%%03, //data
+                         0/00%%0c); //mgt
+""" % (args.dev, args.mon)
+
+print """
+
+wifi_cl [0] -> Discard;
+
+wifi_cl [1]
+    -> mgt_cl :: Classifier(0/a0%f0, // disassoc
+                        0/c0%f0, // deauth
+                        -);
+
+mgt_cl[0] 
+    -> Print(Disassoc) 
+    -> [1]sdn_agent;
+
+mgt_cl[1] 
+    -> Print(Deauth) 
+    -> [1]sdn_agent;
+
+mgt_cl[2] -> Discard;
+
+// OUTBOUND = true means capture packets from two directions
+FromDevice(wlan1, OUTBOUND true)
+    -> Classifier(12/0800)
+    -> CheckIPHeader(14, CHECKSUM true)
+    -> ip_class :: IPClassifier(icmp type echo-reply, dst udp port 67, dst udp port 6777, -)
+
+ip_class[0]
+    -> [4]sdn_agent; 
+    // -> [1]server_offer::DHCPServerOffer(leases);
+
+// dhcp packets
+ip_class[1]
+    -> CheckDHCPMsg
+    -> dhcp_class :: DHCPClassifier( discover, request, release, -);
+
+dhcp_class[0] -> Print(DISCOVER) 
+    -> [0]server_offer::DHCPServerOffer(leases);
+
+dhcp_class[1] -> Print(REQUEST)
+    ->[2]sdn_agent;
+    // -> [2]sdn_agent[1]
+    // -> q::Queue(1000)
+    // -> to_dev::ToDevice(wlan1);
+
+dhcp_class[2] -> Print(RELEASE) 
+    -> DHCPServerRelease(leases);
+     
+dhcp_class[3] -> Print(OTHER) -> Discard;
+
+ip_class[2]
+    -> Print
+    -> [3]sdn_agent;
+
+ip_class[3]
+    -> [5]sdn_agent[2]
+    -> Discard;
+
+server_offer[0]
+    -> q;
+"""
